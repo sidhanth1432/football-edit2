5ab4cf44567ab6b126ac0bf393c7bd38e532a4b5
sidhanth1432 <73776554+sidhanth1432@users.noreply.github.com>
2020-12-08 23:02:04 -0800

    Update functionalities.cpp

--- functionalities.cpp
+++ functionalities.cpp
@@ -2,9 +2,9 @@
 // Created by kunal on 4/3/18.
 //
 
-#include <GL/glut.h>
-#include <GL/glu.h>
-#include <GL/freeglut.h> 
+#include <usr/include/GL/glut.h>
+
+#include <usr/include/GL/freeglut.h> 
 
 
 
