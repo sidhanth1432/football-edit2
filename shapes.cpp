4be158be211dc3d3fbe5e8eb39471f95006c34fd
sidhanth1432 <73776554+sidhanth1432@users.noreply.github.com>
2020-12-08 23:59:44 -0800

    Update shapes.cpp

--- shapes.cpp
+++ shapes.cpp
@@ -2,7 +2,7 @@
 #include <GL/glut.h>
 #include <GL/freeglut.h>
 #include <GL/glu.h>
-#include <GL/gl.h>
+#include <GL/glext.h>
 #include "shapes.h"
 #include <bits/stdc++.h>
 #include "functionalities.h"
